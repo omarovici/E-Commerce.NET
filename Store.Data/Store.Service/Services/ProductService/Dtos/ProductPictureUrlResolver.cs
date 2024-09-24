9007ec4d8f18d92137eb5795b013ddfe51be757c
omarovici <omarkhalidsaber042004@gmail.com>
2024-09-24 18:18:31 +0300

    Profile Picture Url Resolver

---
+++ Store.Data/Store.Service/Services/ProductService/Dtos/ProductPictureUrlResolver.cs
@@ -0,0 +1,21 @@
+using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Store.Data.Entities;
+
+namespace Store.Service.Services.ProductService.Dtos;
+
+public class ProductPictureUrlResolver : IValueResolver<Product,ProductDetailsDto,string>
+{
+    private readonly IConfiguration _configuration;
+    public ProductPictureUrlResolver(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    public string Resolve(Product source, ProductDetailsDto destination, string destMember, ResolutionContext context)
+    {
+        if(!string.IsNullOrEmpty(source.PictureUrl))
+            return $"{_configuration["BaseUrl"]}{source.PictureUrl}";
+        
+        return null;
+    }
+}
--- Store.Data/Store.Service/Services/ProductService/Dtos/ProductProfile.cs
+++ Store.Data/Store.Service/Services/ProductService/Dtos/ProductProfile.cs
@@ -9,7 +9,8 @@ public class ProductProfile : Profile
     {
         CreateMap<Product, ProductDetailsDto>()
             .ForMember(dest => dest.BrandName, options => options.MapFrom(src => src.Brand.Name))
-            .ForMember(dest => dest.TypeName, options => options.MapFrom(src => src.Type.Name));
+            .ForMember(dest => dest.TypeName, options => options.MapFrom(src => src.Type.Name))
+            .ForMember(dest => dest.PictureUrl, options => options.MapFrom<ProductPictureUrlResolver>());
 
         CreateMap<ProductBrand, BrandTypeDetailsDto>();
         CreateMap<ProductType, BrandTypeDetailsDto>();
