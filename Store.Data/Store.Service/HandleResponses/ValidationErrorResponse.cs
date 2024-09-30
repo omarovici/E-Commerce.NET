3dc395f8a2e590ea1ee8810a5000fc36d36fcfae
omarovici <omarkhalidsaber042004@gmail.com>
2024-09-30 13:10:48 +0300

    Handle Responses

---
+++ Store.Data/Store.Service/HandleResponses/ValidationErrorResponse.cs
@@ -0,0 +1,9 @@
+namespace Store.Service.HandleResponses;
+
+public class ValidationErrorResponse : CustomException
+{
+    public ValidationErrorResponse() : base(400)
+    {
+    }
+    public IEnumerable<string> Errors { get; set; }
+}
