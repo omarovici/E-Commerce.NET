81d140238a947318f955757b5a2b17a5f075ca5a
omarovici <omarkhalidsaber042004@gmail.com>
2024-10-02 18:28:36 +0300

    Basket Repository Setup

---
+++ Store.Data/Store.Repository/Basket/IBasketRepository.cs
@@ -0,0 +1,10 @@
+using Store.Repository.Basket.Models;
+
+namespace Store.Repository.Basket;
+
+public interface IBasketRepository
+{
+    Task<CustomerBasket> GetBasketAsync(string basketId);
+    Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket);
+    Task<bool> DeleteBasketAsync(string basketId);
+}
