2c8c1200c37a4884a93c42d26e94164a69b7706c
omarovici <omarkhalidsaber042004@gmail.com>
2024-10-08 17:09:39 +0300

    Swagger Documentation

---
+++ Store.Data/Store.Web/Extensions/SwaggerServiceExtension.cs
@@ -0,0 +1,48 @@
+using Microsoft.OpenApi.Models;
+
+namespace Store.Web.Extensions;
+
+public static class SwaggerServiceExtension
+{
+
+    public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc(
+                "v1",
+                new OpenApiInfo
+                {
+                    Title = "Store API",
+                    Version = "v1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Omar",
+                        Email = "omarkhalidsaber042004@gmail.com",
+                        Url = new Uri("https://x.com/omarovici")
+                    }
+                });
+            var securitySchema = new OpenApiSecurityScheme
+            {
+                Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "bearer",
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            };
+
+            options.AddSecurityDefinition("Bearer", securitySchema);
+            var securityRequirement = new OpenApiSecurityRequirement
+            {
+                { securitySchema, new[] { "Bearer" } }
+            };
+            options.AddSecurityRequirement(securityRequirement);
+        });
+        return services;
+    }
+}
--- Store.Data/Store.Web/Program.cs
+++ Store.Data/Store.Web/Program.cs
@@ -43,7 +43,7 @@ public class Program
         
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerDocumentation();
 
         var app = builder.Build();
         await ApplySeeding.ApplySeedingAsync(app);
